ef4709422ba5de72b64775710248cf0476b97793
andrew <andrew_mcgrogan@yahoo.com>
2022-12-29 16:59:47 -0500

    starting fresh

--- bundle.js
+++ bundle.js
@@ -15,16 +15,6 @@ submitButton.addEventListener("click", function () {
     method: 'get',
     url: `${baseURL}?owner=${ownerAddr}`
   };
-  (0, _axios.default)(config).then(function (response) {
-    console.log(response.data);
-    const REPLACEWITHNAMEOFNFT = response.data.ownedNfts[NUMBEROFNFT].metadata.name;
-    for (let i = 0; i < response.data.ownedNfts.length; i++) {
-      const nft = response.dta.ownedNfts[i];
-      // create a NEW html elemnt, insert it into the page, in the "all-nfts" div
-      // const myelement = document.createElement("img")
-      // img.src = nft.media.image;
-    }
-  });
 });
 
 },{"axios":2}],2:[function(require,module,exports){
--- index.js
+++ index.js
@@ -1,30 +1,3 @@
 import axios from "axios";
-
-console.log(axios);
-
-const submitButton = document.getElementById("submit-button");
-
-submitButton.addEventListener("click", function() {
-const addressinput = document.getElementById("address-input");
-console.log(addressinput.value);
-
-const apiKey = "eqVlGAkmbuEJsJ_9jR5c9RAsVA8yYRBy";
-const baseURL = `https://eth-mainnet.g.alchemy.com/nft/v2/${apiKey}/getNFTs/`;
-const ownerAddr = addressinput.value;
-
-var config = {
-    method: 'get',
-    url: `${baseURL}?owner=${ownerAddr}`
-};
-
-axios(config).then(function(response) {
-    console.log(response.data);
-    const REPLACEWITHNAMEOFNFT = response.data.ownedNfts[NUMBEROFNFT].metadata.name;
-for(let i=0; i < response.data.ownedNfts.length;i++) {
-    const nft = response.data.ownedNfts[i];
-    // create a NEW html elemnt, insert it into the page, in the "all-nfts" div
-    // const myelement = document.createElement("img")
-    // img.src = nft.media.image;
-}
-})
-})
+// we use `import axios from "axios"` which is another way of saying `const axios = require("axios")`
+// it is jsut better supported in the browser!
